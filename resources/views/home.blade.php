2483321b681686e484325a8b8786a68625f5b351
Nicowski25 <nicola.faedo0610@gmail.com>
2023-05-25 16:15:27 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Migration Seeder</title>
+</head>
+<body>
+    <h1>Migration Seeder</h1>
+</body>
+</html>
